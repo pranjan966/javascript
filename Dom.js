e6378357b01db322d2115e7a5ff511629ff856e4
pranjan966 <108427201+pranjan966@users.noreply.github.com>
2023-07-04 16:32:33 +0530

    creating element using js

--- Dom.js
+++ Dom.js
@@ -44,20 +44,86 @@
 
 
 // quearyselectorall
-const items = document.querySelectorAll(".list-group-item")
-console.log(items)
-items[1].style.color = "#00FF00"
-const itemall = document.querySelectorAll(".list-group-item:nth-child(odd)")
-for (let i = 0; i < items.length; i++) {
-    itemall[i].style.backgroundColor = 'green'
+// const items = document.querySelectorAll(".list-group-item")
+// console.log(items)
+// items[1].style.color = "#00FF00"
+// const itemall = document.querySelectorAll(".list-group-item:nth-child(odd)")
+// for (let i = 0; i < items.length; i++) {
+//     itemall[i].style.backgroundColor = 'green'
 
 
-    //aa
-}
+//     //aa
+// }
 
 
 
 
 // const li = document.getElementsByTagName('li')
 // li[4].style.color = 'red'
-///abcd
+
+
+
+// Traversing DOM
+const listItems = document.querySelector('#items')
+// listItems.parentNode.style.background = '#f4f4f4'
+// listItems.parentNode.parentNode.style.background = '#ccc'
+// listItems.parentNode.parentNode.parentNode.style.background = "green"
+
+// console.log(listItems.parentNode)
+// // console.log(listItems.parentNode.parentNode.parentNode)
+
+// const listelements = document.querySelector('#items')
+// console.log(listelements.parentElement)
+// console.log(listelements.parentElement.parentElement.parentElement)
+// listelements.parentElement.style.background = '#f4f4f4'
+// listelements.parentElement.parentElement.style.background = '#ccc'
+// listelements.parentElement.parentElement.parentElement.style.background = "green"
+
+// childnode
+// console.log(listItems.childNodes)(contains text )
+// console.log(listItems.children)
+// listItems.children[2].style.background = 'yellow'
+
+// console.log(listItems.firstChild)
+// console.log(listItems.firstElementChild)
+// listItems.firstElementChild.style.background = '#ccc'
+
+// console.log(listItems.lastChild)
+// console.log(listItems.lastElementChild)
+// listItems.lastElementChild.style.background = "green"
+
+
+// // nextsibling
+// console.log(listItems.nextSibling)
+// // console.log(listItems.nextElementSibling)(we dont have)
+
+// console.log(listItems.previousSibling)
+// console.log(listItems.previousElementSibling)
+
+
+// creating element
+const newDiv = document.createElement('div')
+console.log(newDiv)
+console.log(newDiv.className = "hello")
+console.log(newDiv.id = "hello")
+newDiv.setAttribute('title', 'hello div')
+
+// create a textNode
+const newDivtext = document.createTextNode("hello world");
+console.log(newDivtext)
+
+// add text to div
+newDiv.appendChild(newDivtext)
+console.log(newDiv)
+
+const container = document.querySelector('header .container');
+// console.log(container)
+const h1 = document.querySelector("header h1");
+// console.log(h1)
+console.log(newDiv)
+container.insertBefore(newDiv, h1)
+console.log(document)
+
+
+
+
