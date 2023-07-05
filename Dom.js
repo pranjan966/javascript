3b575bb464cb90b19f8754fc957235cc95488038
pranjan966 <108427201+pranjan966@users.noreply.github.com>
2023-07-05 17:03:32 +0530

    added function to button

--- Dom.js
+++ Dom.js
@@ -64,7 +64,7 @@
 
 
 // Traversing DOM
-const listItems = document.querySelector('#items')
+// const listItems = document.querySelector('#items')
 // listItems.parentNode.style.background = '#f4f4f4'
 // listItems.parentNode.parentNode.style.background = '#ccc'
 // listItems.parentNode.parentNode.parentNode.style.background = "green"
@@ -101,29 +101,91 @@ const listItems = document.querySelector('#items')
 // console.log(listItems.previousElementSibling)
 
 
-// creating element
-const newDiv = document.createElement('div')
-console.log(newDiv)
-console.log(newDiv.className = "hello")
-console.log(newDiv.id = "hello")
-newDiv.setAttribute('title', 'hello div')
+// // creating element
+// const newDiv = document.createElement('div')
+// console.log(newDiv)
+// console.log(newDiv.className = "hello")
+// console.log(newDiv.id = "hello")
+// newDiv.setAttribute('title', 'hello div')
 
-// create a textNode
-const newDivtext = document.createTextNode("hello world");
-console.log(newDivtext)
+// // create a textNode
+// const newDivtext = document.createTextNode("hello world");
+// console.log(newDivtext)
 
-// add text to div
-newDiv.appendChild(newDivtext)
-console.log(newDiv)
+// // add text to div
+// newDiv.appendChild(newDivtext)
+// console.log(newDiv)
 
-const container = document.querySelector('header .container');
-// console.log(container)
-const h1 = document.querySelector("header h1");
-// console.log(h1)
-console.log(newDiv)
-container.insertBefore(newDiv, h1)
-console.log(document)
-
-
+// const container = document.querySelector('header .container');
+// // console.log(container)
+// const h1 = document.querySelector("header h1");
+// // console.log(h1)
+// console.log(newDiv)
+// container.insertBefore(newDiv, h1)
+// console.log(document)
 
 
+// accesing element if form and listitems
+const form = document.getElementById('addForm');
+const listItems = document.getElementById("items");
+const filter = document.getElementById('filter')
+
+// form submit event
+form.addEventListener("submit", addItems);
+listItems.addEventListener("click", removeItem)
+filter.addEventListener("keyup", filterItems)
+
+// add items
+
+function addItems(e) {
+    e.preventDefault()
+    // 
+    newItem = document.getElementById("item").value;
+
+    // creating new li element
+    const li = document.createElement("li");
+    // add class nameto new li
+    li.className = "list-group-item";
+    // add textNode with input value
+    li.appendChild(document.createTextNode(newItem));
+    // creating delete element
+    const deleteBtn = document.createElement("button");
+    //adding class name to delete button
+    deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+    // adding text to button
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    // append butoon to li
+    li.appendChild(deleteBtn);
+    // append li to list
+    listItems.appendChild(li);
+
+
+}
+
+
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are you sure')) {
+            const li = e.target.parentElement;
+            listItems.removeChild(li);
+        }
+    }
+
+}
+
+function filterItems(e) {
+    // convert the text to lowercase
+    const text = e.target.value.toLowerCase();
+    // get li
+    const items = listItems.getElementsByTagName('li')
+    // convert to an array
+    Array.from(items).forEach(function (item) {
+        const itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    })
+}
